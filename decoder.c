5c104346be481f56a82f4166c40075e0df77d375
Misufoil <47031854+Misufoil@users.noreply.github.com>
2021-01-29 16:47:36 +0300

    gut

---
+++ decoder.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include "stringutils.h"
+#include "coders.h"
+
+char*   argvReader(int argc,char* argv[]) {
+    int len = 0;
+
+    for (int i = 2; i < argc - 1; ++i) {
+        len += strlen(argv[i]) + 1;
+    }
+    char* str = malloc((len + 1) * (sizeof(char)));
+    
+    for (int i = 2; i < argc - 1; ++i) {
+    strcat(str, argv[i]);
+    strcat(str, " ");
+    }
+    return str;
+}
+
+
+int main(int argc, char* argv[]) {
+    char* command = argv[0];
+    char* method = argv[1];
+    char* str = argvReader(argc, argv);
+    char* key = argv[argc - 1];
+
+    if (strcmp(method, "--caesar") == 0) {
+        muttableEndStrip(str);
+        mutableToLower(str);
+        muttableCaesarDecoder(str, atoi(key));
+	    printf("%s\n", str);
+    }
+    if (strcmp(method, "--xor") == 0) {
+        muttableEndStrip(str);
+        mutableToLower(str);
+        muttableXorCoder(str, key);
+        printf("%s\n", str);
+    }
+        
+    else {
+	    printf("Incorrect data");
+    }
+
+    return 0;
+}
